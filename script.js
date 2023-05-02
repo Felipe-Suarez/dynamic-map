1bb459f2fd4f361b0d6514bf8fcd50024f0106df
Felipe Suarez <felipesuarez1525@gmail.com>
2023-05-02 13:03:59 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const miPath = document.getElementById("markerMap");
+const atts = miPath.getAttribute('d').split('z')
+miPath.removeAttribute('d')
+atts.pop()
+const finalAtts = atts.map(att => att + 'z')
+
+const random = () => Math.floor((Math.random() * finalAtts.length))
+
+const getAtts = (n) => n.map(num => finalAtts[num])
+
+setInterval(() => {
+    const randoms = []
+    for (let i = 0; i < 8; i++) {
+        randoms.push(random())
+    }
+
+    miPath.setAttribute('d', getAtts(randoms).join(''))
+}, 1000)
